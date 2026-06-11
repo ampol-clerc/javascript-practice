b36af737f4230d1017db1061c569cc2f9b280138
ampol-clerc <ampolclerc@gmail.com>
2026-06-11 15:24:17 +0200

    build variables project

---
+++ 01-variables/script.js
@@ -0,0 +1,11 @@
+const firstName = "Ampol";
+let age = 29;
+let city = "Lausanne";
+const country = "Switzerland";
+let profession = "Future Full Stack Developer";
+
+console.log("Name: " + firstName);
+console.log("Age: " + age);
+console.log("Living in city: " + city);
+console.log("Country: " + country);
+console.log("Career Goal: " + profession);
