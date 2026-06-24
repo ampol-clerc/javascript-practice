3b1cc756a765e196a4ccb6672fa5cc3ac35c3be3
ampol-clerc <ampolclerc@gmail.com>
2026-06-24 12:58:01 +0200

    build arithmetic project

---
+++ 03-arithmetics/script.js
@@ -0,0 +1,9 @@
+// Basic Arithmetic and Modulo Practice
+var totalCandy = 10;
+var people = 3;
+var remainder = totalCandy % people;
+
+console.log("Total Candies: " + totalCandy);
+console.log("People: " + people);
+console.log("Remainder Candy: " + remainder);
+alert("Remainder Candy: " + remainder);
