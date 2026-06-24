a72d49d1b09637465b63af8555af009a0c40fc05
ampol-clerc <ampolclerc@gmail.com>
2026-06-24 13:01:01 +0200

    feat: add basic arithmetic and operator exercise

--- 03-arithmetics/script.js
+++ 03-arithmetics/script.js
@@ -7,3 +7,8 @@ console.log("Total Candies: " + totalCandy);
 console.log("People: " + people);
 console.log("Remainder Candy: " + remainder);
 alert("Remainder Candy: " + remainder);
+
+// Arithmetic and operator exercise
+var dogAge = prompt("How old is your dog?");
+var humanAge = ((dogAge - 2) * 4) + 21;
+alert("Your dog is " + humanAge + " years old in human years.");
