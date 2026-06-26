f147610863a4b50a884e10047073d9278ca50dbd
ampol-clerc <ampolclerc@gmail.com>
2026-06-26 16:44:48 +0200

    feat: implement lifeInWeeks function using parameters and arguments

--- 04-functions/script.js
+++ 04-functions/script.js
@@ -11,3 +11,19 @@ function getMilk() {
 }
 
 getMilk();
+
+// Part 2: Functions with Parameters and Arguments
+
+function lifeInWeeks(age) {
+    var yearLeft = 90 - age;
+    var dayLeft = yearLeft * 365;
+    var weekLeft = yearLeft * 52;
+    var monthLeft = yearLeft * 12;
+
+    console.log("Input Age: " + age);
+    console.log("You have " + dayLeft + " days, " + weekLeft + " weeks, and " + monthLeft + " months left.");
+
+    //alert("You have " + dayLeft + " days, " + weekLeft + " weeks, and " + monthLeft + " months left.");
+}
+
+lifeInWeeks(29);
