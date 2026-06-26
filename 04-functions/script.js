bd21c883fafb5b35922ca13a24c7446833212f59
ampol-clerc <ampolclerc@gmail.com>
2026-06-26 16:24:27 +0200

    feat: add basic function declaration and execution block

---
+++ 04-functions/script.js
@@ -0,0 +1,13 @@
+// Part 1: Creating and Calling Functions
+
+function getMilk() {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("buy 12 bottles of milk");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk();
