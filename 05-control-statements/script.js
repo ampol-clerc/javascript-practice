baaf026e0a8e8e6bf27de9f6cb886439ff99e352
ampol-clerc <ampolclerc@gmail.com>
2026-06-29 17:29:00 +0200

    feat: implement love calculator with random number generation

--- 05-control-statements/script.js
+++ 05-control-statements/script.js
@@ -3,3 +3,17 @@ var n = Math.random();
 n = n * 6;
 n = Math.floor(n);
 console.log("Basic Random Number (0-5): " + n);
+
+// The Love Calculator Feature
+var yourName = prompt("Your Name: ");
+var yourCrush = prompt("Your Crush: ");
+
+console.log("User Name: " + yourName);
+console.log("Crush Name: " + yourCrush);
+
+// Random Number (1 - 100)
+var loveCalculator = Math.random() * 100;
+loveCalculator = Math.floor(loveCalculator) + 1;
+
+console.log("Calculates Love Score: " + loveCalculator + "%");
+alert(yourName + " & " + yourCrush + " love score is " + loveCalculator + "%");
