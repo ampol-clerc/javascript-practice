499a3d18e7e5270cb143c8a6cc26b129e7e1ac03
ampol-clerc <ampolclerc@gmail.com>
2026-06-30 13:37:49 +0200

    feat: implement bmi calculator with decimal formatting

--- 05-control-statements/script.js
+++ 05-control-statements/script.js
@@ -1,37 +1,38 @@
-// Random Number Generation
-var n = Math.random();
-n = n * 6;
-n = Math.floor(n);
-console.log("Basic Random Number (0-5): " + n);
+// // Random Number Generation
+// var n = Math.random();
+// n = n * 6;
+// n = Math.floor(n);
+// console.log("Basic Random Number (0-5): " + n);
 
-// The Love Calculator Feature
-var yourName = prompt("Your Name: ");
-var yourCrush = prompt("Your Crush: ");
+// // The Love Calculator Feature
+// var yourName = prompt("Your Name: ");
+// var yourCrush = prompt("Your Crush: ");
 
-console.log("User Name: " + yourName);
-console.log("Crush Name: " + yourCrush);
+// console.log("User Name: " + yourName);
+// console.log("Crush Name: " + yourCrush);
 
-// Random Number (1 - 100)
-var loveCalculator = Math.random() * 100;
-loveCalculator = Math.floor(loveCalculator) + 1;
+// // Random Number (1 - 100)
+// var loveCalculator = Math.random() * 100;
+// loveCalculator = Math.floor(loveCalculator) + 1;
 
-console.log("Calculates Love Score: " + loveCalculator + "%");
-alert(yourName + " & " + yourCrush + " love score is " + loveCalculator + "%");
+// console.log("Calculates Love Score: " + loveCalculator + "%");
+// alert(yourName + " & " + yourCrush + " love score is " + loveCalculator + "%");
 
 
 // BMI Calculator Advanced (IF/ELSE)
 function bmiCalculator(weight, height) {
     var bmi = weight / Math.pow(height, 2);
+    var bmiShort = bmi.toFixed(1);
     var interpretation;
 
     if (bmi < 18.5) {
-        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+        interpretation = "Your BMI is " + bmiShort + ", so you are underweight.";
     }
     else if (bmi >= 18.5 && bmi <= 24.9) {
-        interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+        interpretation = "Your BMI is " + bmiShort + ", so you have a normal weight.";
     }
     else {
-        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+        interpretation = "Your BMI is " + bmiShort + ", so you are overweight.";
     }
     
     return interpretation;
