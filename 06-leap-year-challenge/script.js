bfacc8e6b588f94bea9f9a83e49e29eef9e16787
ampol-clerc <ampolclerc@gmail.com>
2026-06-30 15:06:18 +0200

    feat: add leap year algorithm with modulo check

--- 06-leap-year-challenge/script.js
+++ 06-leap-year-challenge/script.js
@@ -1,1 +1,27 @@
 // Leap Year Challenge Exercise
+
+function isLeap(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return "Leap year.";
+            }
+            else {
+                return "Not leap year.";
+            }
+        }
+        else {
+            return "Leap year.";
+        }
+    }
+    else {
+        return "Not leap year.";
+    }
+}
+
+// Test Function
+var checkYear = prompt("Enter Year ?");
+var result = isLeap(checkYear);
+
+console.log("Is " + checkYear + " a leap year?: " + result);
+alert(checkYear + " IS a " + result);
