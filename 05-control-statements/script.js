969a9fbc8a142ad32e5bb3a2db6ad8069bfedddd
ampol-clerc <ampolclerc@gmail.com>
2026-06-30 13:32:28 +0200

    feat: implement advanced bmi calculator using if-else conditionals

--- 05-control-statements/script.js
+++ 05-control-statements/script.js
@@ -17,3 +17,26 @@ loveCalculator = Math.floor(loveCalculator) + 1;
 
 console.log("Calculates Love Score: " + loveCalculator + "%");
 alert(yourName + " & " + yourCrush + " love score is " + loveCalculator + "%");
+
+
+// BMI Calculator Advanced (IF/ELSE)
+function bmiCalculator(weight, height) {
+    var bmi = weight / Math.pow(height, 2);
+    var interpretation;
+
+    if (bmi < 18.5) {
+        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+    }
+    else if (bmi >= 18.5 && bmi <= 24.9) {
+        interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+    }
+    else {
+        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+    }
+    
+    return interpretation;
+}
+
+var bmiResult = bmiCalculator(61, 1.74);
+console.log("BMI Interpretation Result: " + bmiResult);
+alert(bmiResult);
