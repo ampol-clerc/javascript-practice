73c950ede45db8829947a32466eef4c386d1e052
ampol-clerc <ampolclerc@gmail.com>
2026-07-01 13:17:57 +0200

    feat: implement fizzbuzz logic with array push method

--- 07-arrays/script.js
+++ 07-arrays/script.js
@@ -13,3 +13,22 @@ function checkGuestName() {
 checkGuestName();
 
 console.log("Guest Name: " + guestName);
+
+
+// Adding Elements and Intermediate Array (Program FizzBuzz)
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+    if (count % 3 === 0 && count % 5 === 0) {
+        output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+        output.push("Fizz");
+    } else if (count % 5 === 0) {
+        output.push("Buzz");
+    } else {
+        output.push(count);
+    }
+    count++;
+    console.log(output);
+}
