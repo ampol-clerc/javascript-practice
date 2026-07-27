40cee6ba2db9627402cb0d8c449451743aee36ce
ampol-clerc <ampolclerc@gmail.com>
2026-07-27 22:03:29 +0200

    build: initialize convert length project

---
+++ 13-convert-length/script.js
@@ -0,0 +1,2 @@
+// Function convertLength(length, from, to) that takes a number and a unit ('km' or 'miles') and converts the length to another unit ('km' or 'miles').
+// Note: 1 mile = 1.6 km (approximately)
