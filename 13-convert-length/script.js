006716d4f6d29ccf70f8806290d8fa51b34de4c9
ampol-clerc <ampolclerc@gmail.com>
2026-07-27 22:18:46 +0200

    feat: add invalid unit check statements and finalize project

--- 13-convert-length/script.js
+++ 13-convert-length/script.js
@@ -2,6 +2,13 @@
 // Note: 1 mile = 1.6 km (approximately)
 
 function convertLength(length, from, to) {
+  // Invalid Check statement
+  if (from !== "km" && from !== "miles") {
+    return `Invalid unit: ${from}`;
+  } else if (to !== "km" && to !== "miles") {
+    return `Invalid unit: ${to}`;
+  }
+
   // Conversion Logic statement
   if (from === "km" && to === "miles") {
     return `${length / 1.6} miles`;
@@ -16,3 +23,8 @@ function convertLength(length, from, to) {
 console.log("50 miles to km = " + convertLength(50, "miles", "km"));
 console.log("32 km to miles = " + convertLength(32, "km", "miles"));
 console.log("50 km to km = " + convertLength(50, "km", "km"));
+
+// Test Invalid check statements
+console.log("50 km to ft = " + convertLength(50, "km", "ft"));
+console.log("50 miles to ft = " + convertLength(50, "miles", "ft"));
+console.log("50 miles to miles = " + convertLength(50, "miles", "miles"));
